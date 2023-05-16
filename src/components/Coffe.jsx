102019abb12ba37ac28a09c9c8f126743dececc1
Felipe Palma <felipeandres_uc@outlook.com>
2023-05-16 11:42:36 -0400

    teoría objetos

---
+++ src/components/Coffe.jsx
@@ -0,0 +1,16 @@
+function Coffe(){
+   
+    const datos = {
+    nombre: "Felipe",
+    edad: "28"
+   }
+
+   return(
+    <div>
+        <h1>{datos.nombre}</h1>
+        <h1>{datos.edad}</h1>
+    </div>
+   )
+   
+}
+export default Coffe;
